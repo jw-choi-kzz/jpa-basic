5ecf9840677bcae30b77ee697b1c27cebb4da227
jw-choi-kzz <141205653+jw-choi-kzz@users.noreply.github.com>
2024-01-05 11:06:41 +0900

    Update UserSaveMain.java

--- jpa-01/src/main/java/main/UserSaveMain.java
+++ jpa-01/src/main/java/main/UserSaveMain.java
@@ -8,9 +8,10 @@ import java.time.LocalDateTime;
 public class UserSaveMain {
     public static void main(String[] args) {
         EntityManagerFactory emf =
-                Persistence.createEntityManagerFactory("jpabegin");//jpabegin은 persistense.xml에서 설정한 영속 단위이름
-
-        EntityManager entityManager = emf.createEntityManager(); //실제 db 연동을 처리
+                Persistence.createEntityManagerFactory("jpabegin"); //영속 단위 기준으로 초기화. 필요한 자원 생성
+        //jpabegin은 persistense.xml에서 정의한 영속 단위 기준 이름
+ 
+        EntityManager entityManager = emf.createEntityManager(); //실제 db 연동을 처리. 한번만 생성.
         EntityTransaction transaction = entityManager.getTransaction();
         try {
             transaction.begin();
@@ -24,6 +25,6 @@ public class UserSaveMain {
             entityManager.close();
         }
 
-        emf.close();
+        emf.close(); //팩토리 닫음. 사용한 자원 반환
     }
 }
