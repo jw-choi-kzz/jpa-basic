25c90b61e7036360079326222b2ab2729e42e699
jw-choi-kzz <141205653+jw-choi-kzz@users.noreply.github.com>
2024-01-05 10:51:00 +0900

    Update User.java

--- jpa-01/src/main/java/jpabasic/reserve/domain/User.java
+++ jpa-01/src/main/java/jpabasic/reserve/domain/User.java
@@ -7,14 +7,14 @@ import jakarta.persistence.Table;
 
 import java.time.LocalDateTime;
 
-@Entity
-@Table(name = "user")
+@Entity //DB 테이블과 매핑할 대상(보통 Entity 단위로 하기 때문에 어노테이션 entity)
+@Table(name = "user") //user 테이블과 매핑
 public class User {
-    @Id
-    private String email;
-    private String name;
+    @Id //식별자에 대응. 보통 table의 PK와 매핑하는 Column에 붙이게 됨
+    private String email; //(이름이 같은) email 칼럼과 매핑
+    private String name; //name 칼럼과 매핑
 
-    @Column(name = "create_date")
+    @Column(name = "create_date") //column과 이름이 다른 경우 어노테이션 Column 사용. name 이름의 column과 매핑 
     private LocalDateTime createDate;
 
     protected User() {
