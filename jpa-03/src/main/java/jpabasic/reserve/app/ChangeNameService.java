eec99d61ee359abe72ce20d11c079605aa021920
jw-choi-kzz <141205653+jw-choi-kzz@users.noreply.github.com>
2024-01-05 12:51:50 +0900

    Update ChangeNameService.java

--- jpa-03/src/main/java/jpabasic/reserve/app/ChangeNameService.java
+++ jpa-03/src/main/java/jpabasic/reserve/app/ChangeNameService.java
@@ -1,5 +1,7 @@
 package jpabasic.reserve.app;
-
+/*
+ * 수정 - 트랜잭션 범위 내에서 변경된 값을 자동 반영
+ */
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 import jpabasic.reserve.domain.User;
@@ -15,7 +17,7 @@ public class ChangeNameService {
             if (user == null) {
                 throw new NoUserException();
             }
-            user.changeName(newName);
+            user.changeName(newName); //수정
             tx.commit();
         } catch(Exception ex) {
             tx.rollback();
