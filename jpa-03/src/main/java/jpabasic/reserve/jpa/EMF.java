ac5a7c17280801d54cc1b6ff9466091e020c0b4c
jw-choi-kzz <141205653+jw-choi-kzz@users.noreply.github.com>
2024-01-05 12:41:11 +0900

    Update EMF.java

--- jpa-03/src/main/java/jpabasic/reserve/jpa/EMF.java
+++ jpa-03/src/main/java/jpabasic/reserve/jpa/EMF.java
@@ -1,5 +1,9 @@
 package jpabasic.reserve.jpa;
-
+/*
+ * EntityManager를 쉽게 구하기 위한 보조 클래스
+ * EntityManager를 계속 들고다녀야 하는 불편함 -> EntityManager를 static으로 담고 있음
+ * EntityManager가 필요하면 createEntityManager 메서드를 사용
+ */
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
