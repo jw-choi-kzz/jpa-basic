8224d744e695e933e920d48248f783ba164ddc73
jw-choi-kzz <141205653+jw-choi-kzz@users.noreply.github.com>
2024-01-05 10:54:26 +0900

    Update UserSaveMain.java

--- jpa-01/src/main/java/main/UserSaveMain.java
+++ jpa-01/src/main/java/main/UserSaveMain.java
@@ -8,14 +8,14 @@ import java.time.LocalDateTime;
 public class UserSaveMain {
     public static void main(String[] args) {
         EntityManagerFactory emf =
-                Persistence.createEntityManagerFactory("jpabegin");
+                Persistence.createEntityManagerFactory("jpabegin");//jpabegin은 persistense.xml에서 설정한 영속 단위이름
 
-        EntityManager entityManager = emf.createEntityManager();
+        EntityManager entityManager = emf.createEntityManager(); //실제 db 연동을 처리
         EntityTransaction transaction = entityManager.getTransaction();
         try {
             transaction.begin();
             User user = new User("user@user.com", "user", LocalDateTime.now());
-            entityManager.persist(user);
+            entityManager.persist(user); //persist 메서드 호출로 user 파라미터가 DB에 저장  
             transaction.commit();
         } catch (Exception ex) {
             ex.printStackTrace();
